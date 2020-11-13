31cc81fa79fb1d6140e7716bb9cfda2a8bc2dfe7
DillonSensenig <70489473+DillonSensenig@users.noreply.github.com>
2020-11-13 09:50:09 -0800

    updated notes, + to checkWin func. + result (win)
    Still need to work out how to detect and display a draw. Removed finished requirements and moved the rest down to make the js look a little cleaner.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Built with HTML and JavaScript, the page should load an empty 3x3 grid roughly the same size as the window of desktop browser. DONE
-
-// **Players will take turns clicking on empty cells of the grid with their mouse to fill the cell with an X or O symbol, one for each player.** //--// **For the first turn, randomize the starting symbol. After that, alternate the player symbol after playing an empty square. The current player should be shown beneath the grid.**
-
 
 // Added currentPlayer variable. Math.floor avoids decimals. Math.random to alternate from X to O
 
@@ -12,16 +8,13 @@ var currentPlayer = Math.floor(Math.random() * 2) == 0 ? "X" : "O";
 function cellChosen(cellNumber) {
   if(document.getElementById(cellNumber.toString()).innerHTML == "") {
     document.getElementById(cellNumber.toString()).innerHTML = currentPlayer;
+    checkWin();
     currentPlayer = currentPlayer == "X" ? "O" : "X";
   }
 };
 
-// The game should detect a winner or a draw and print the result beneath the grid.
-
-// A win is 3 consecutive symbols in a row, a column, or either diagonal through the center.
-
-// A draw is a filled grid without a winner.
 //create array of winning combinations
+
 const winner = [
   [0, 1, 2],
   [3, 4, 5],
@@ -33,18 +26,39 @@ const winner = [
   [2, 4, 6]
 ]
 
-// loop through each winning combination. Then, it needs to loop through the 3 cell numbers and check if they're the same
-// create a boolean variable that keeps track if each cell is the same in the combination
-// start as true, but if it ever is not the same, become false, and go to the next combination
-// if it finds any, prevent further play (use a global variable that disables cellchosen) and print winner
-// check if every cell is full, and if they are, and there is no winner, then it needs to print out that it's a draw
-
-function checkWin(winner) {
-  if (winner === currentPlayer);
-  console.log("You win!");
+// function to check for a winner. Loops through array and cells to see if one matches the other. Returns "result". Boolean to check if said is true/false
+function checkWin() {
+  var foundWin = "";
+  for (let i = 0; i < winner.length; i++) {
+    var canBeWin = true;
+    for (let j = 0; j < winner[i].length; j++) {
+      if(document.getElementById(winner[i][j].toString()).innerHTML != currentPlayer) {
+        canBeWin = false;
+        break;
+      }
+    }
+    if(canBeWin) {
+       foundWin = currentPlayer; 
+        return (document.getElementById("result"))
+        .innerHTML ="Winner!" 
+       break;
+    }
+  }
 }
 
 
+// resetGame function loops through cells to change them back to empty using innerHTML. Also changes result back to "Win or Draw"
+function resetGame() {
+  for(i = 0; i < 9; i++) {
+  document.getElementById(i.toString()).innerHTML = "";
+  document.getElementById("result").innerHTML = "Win or Draw";
+ }
+}
+
+  
+// The game should detect a winner or a draw and print the result beneath the grid. <--- Need draw still
+
+// A draw is a filled grid without a winner. <--- Need
 
 // Bonus: The grid should no longer be playable after the game ends, but you could create a button to reset the grid to play a second game. The losing symbol should go first.
 
@@ -52,15 +66,8 @@ function checkWin(winner) {
 
 //remove "X's" and "O's"
 //restart game
-
 //const restart = document.querySelector(".restart");
 //restart.addEventListener("click", () => {
 //});
 
-// Function that removes XO's inside of the cells after clicking the restart button. Previous iteration broke the onClick for the cells.
-function resetGame() {
-  for(i = 0; i < 9; i++) {
-  document.getElementById(i.toString()).innerHTML = "";
- }
-}
 
